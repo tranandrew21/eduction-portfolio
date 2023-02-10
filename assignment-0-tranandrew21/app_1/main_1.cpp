c54be0dfef58ebd5123e882598db63bd259dd72f
tranandrew21 <110001270+tranandrew21@users.noreply.github.com>
2023-02-10 09:10:40 -0700

    Delete assignment-0-tranandrew21 directory

--- assignment-0-tranandrew21/app_1/main_1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-
-using namespace std;
-
-int main(int argc, char* argv[]){
-    cout << "i'm a computer\n";
-    return 0;
-}
--- assignment-0-tranandrew21/build/CMakeFiles/run_tests_1.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for run_tests_1.
-# This may be replaced when dependencies are built.
