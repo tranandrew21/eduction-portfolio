b956d3a3a06bfe08c177e0f6204c7ee006ca159f
tranandrew21 <110001270+tranandrew21@users.noreply.github.com>
2023-02-10 09:11:04 -0700

    Delete assignment-2-tranandrew21 directory

--- assignment-2-tranandrew21/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jovyan/assignment-2-tranandrew21
-# Build directory: /home/jovyan/assignment-2-tranandrew21/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
